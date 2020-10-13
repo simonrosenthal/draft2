d4562e43ee9bb8028b49af87045410a59ce538b8
Wren Miles <72320514+Wrendm@users.noreply.github.com>
2020-10-13 12:54:46 -0700

    Update api_route2.py

--- api_route2.py
+++ api_route2.py
@@ -1,6 +1,7 @@
 import requests
+import time
 
-def getStreetNameFromApi(point):
+def getStreetNameFromApi(point):#, route):
         # print (point.lat, point.lon)
         parameters = {
             "apiKey": "737eceecff3e4e79bdcb66de0e4173e3",
@@ -13,6 +14,9 @@ def getStreetNameFromApi(point):
         response = requests.get(
             "https://geoservices.tamu.edu/Services/ReverseGeocoding/Webservice/v04_01/HTTP/default.aspx",
             params=parameters)
-        print(response.json())
-        addy = response.json()['StreetAddresses']
+        addy = response.json()['StreetAddresses'][0]['StreetAddress']
+        print(addy)
+        #route.apiCalls = route.apiCalls + 1
+        #print(route.apiCalls)
+        #time.sleep(2)
         return addy
