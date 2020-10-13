6eb09d0afa4846867c1937e90fa2f2f107439128
Wren Miles <72320514+Wrendm@users.noreply.github.com>
2020-10-13 11:49:55 -0700

    Update api_route2.py
    works now I guess, also changed the api key

--- api_route2.py
+++ api_route2.py
@@ -3,7 +3,7 @@ import requests
 def getStreetNameFromApi(point):
         # print (point.lat, point.lon)
         parameters = {
-            "apiKey": "eb6a7ee7e8ad43f69ea61d99c1b28daa",
+            "apiKey": "737eceecff3e4e79bdcb66de0e4173e3",
             "version": "4.10",
             "lat": point.lat,
             "lon": point.lon,
@@ -14,5 +14,5 @@ def getStreetNameFromApi(point):
             "https://geoservices.tamu.edu/Services/ReverseGeocoding/Webservice/v04_01/HTTP/default.aspx",
             params=parameters)
         print(response.json())
-        addy = response.json()['StreetAddresses'][0]['StreetAddress']
+        addy = response.json()['StreetAddresses']
         return addy
