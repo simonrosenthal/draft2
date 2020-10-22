6090d45e2611d32c926824fb62f87e41f72c640d
Wren Miles <72320514+Wrendm@users.noreply.github.com>
2020-10-22 15:54:34 -0700

    Update gpx_route2.py
    works.. more now... fewer random side roads.. longer runtime

--- gpx_route2.py
+++ gpx_route2.py
@@ -9,11 +9,19 @@ class Route():
         self.turns = []
         self.pntCount = 0
         self.turnCount = 0
+        self.lastFiltered = 0
 
     def add_point(self, lat, lon):
-        self.pntCount += 1
         point = Point(lat, lon, self.pntCount)
         self.points.append(point)
+        self.pntCount += 1
+        print("last filtered is: " + str(self.lastFiltered))
+        print("adding " + self.points[self.pntCount-1].get_roadname() + " at " + str(self.points[self.pntCount-1].index))
+        if self.lastFiltered > 0 and self.points[self.pntCount-1].get_roadname() != self.points[self.pntCount - 2].get_roadname():
+            print("filtering because " + self.points[self.pntCount-1].get_roadname() + " != " + self.points[self.pntCount - 2].get_roadname())
+            self.double_penetration_filter_points()
+        elif self.points[self.pntCount-1].get_roadname() != self.points[self.pntCount - 2].get_roadname():
+            self.lastFiltered = self.pntCount - 2
 
     def add_turnpoint(self, point):
         newTurn = Turn(point.index)
@@ -32,8 +40,32 @@ class Route():
             for segment in track.segments:
                 for gpxPoint in segment.points:
                     lat = Decimal(gpxPoint.latitude)
-                    long = Decimal(gpxPoint.longitude)
-                    self.add_point(lat, long)
+                    lon = Decimal(gpxPoint.longitude)
+                    self.add_point(lat, lon)
+
+    def double_penetration_filter_points(self):
+        current = self.points[self.lastFiltered + 1].get_roadname()
+        print (current + " at " + str(self.points[self.lastFiltered + 1].index) + " with point count " + str(self.pntCount))
+        indices =  self.pntCount - self.lastFiltered - 2
+        start = self.points[self.lastFiltered]
+        end = self.points[self.lastFiltered + indices + 1]
+        print("filtering begins at " + start.get_roadname() + " and ends at " + end.get_roadname())
+        print("# of indices for " + current +  " is " + str(indices))
+        if start.get_roadname() == end.get_roadname():
+            print(start.get_roadname() + " == " + end.get_roadname())
+            del self.points[(start.index + 1):end.index]
+            self.pntCount = self.pntCount-indices
+            print("deleting the points " + str(start.index) + " through " + str(end.index) + " on " +  current)
+            print("last filtered is " + str(self.lastFiltered))
+            self.points[-1].index = start.index + 1
+            print("last point is: " + self.points[-1].get_roadname() + " at " + str(self.points[-1].index))
+            print ("point count " + str(self.pntCount))        
+        else:
+            self.lastFiltered = self.lastFiltered + indices 
+            
+
+
+
 
 class Point():
 
@@ -61,5 +93,6 @@ class Turn():
         self.direction = ""
         self.turnName = ""
         self.distance = None
+
 
 
